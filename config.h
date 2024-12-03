54791885f7d797d36dd715f096c3facee3309b69
Xenoxsite <xenoxsite@gmail.com>
2024-12-03 17:47:13 +0600

    Update config.h

--- config.h
+++ config.h
@@ -132,7 +132,7 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_w,                     spawn,          SHCMD("set-wallpaper") },
 	{ MODKEY,                       XK_z,                     spawn,          SHCMD("rofi -show run") },
-	{ MODKEY|ShiftMask,             XK_x,             	  spawn,          SHCMD("scrot")},
+	{ MODKEY|ShiftMask,             XK_s,           	  spawn,          SHCMD("scrot")},
 	{ MODKEY,                       XK_Return,                spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_n,          	          spawn,          SHCMD("st -c neovim -e nvim")},
 	{ShiftMask,                     XK_F12,                   spawn,          {.v = volumecmd[0]} },
